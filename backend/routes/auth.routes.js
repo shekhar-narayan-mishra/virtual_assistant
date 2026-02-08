a39ff75dc1a43b7cbf6c096749558c4360da146a
shekhar-narayan-mishra <shekhar.mishra@adypu.edu.in>
2026-02-08 11:00:00 +0530

    feat(backend): setup auth routes

---
+++ backend/routes/auth.routes.js
@@ -0,0 +1,14 @@
+import express from "express"
+import { Login, logout, signup, updateProfile, googleLogin, githubLogin } from "../controllers/auth.controller.js"
+import upload from "../middlewares/upload.middleware.js"
+
+const authRouter = express.Router()
+
+authRouter.post("/signup", signup)
+authRouter.post("/signin", Login)
+authRouter.post("/google-login", googleLogin)
+authRouter.post("/github-login", githubLogin)
+authRouter.get("/Logout", logout)
+authRouter.post("/update-profile", upload.single('assistantImage'), updateProfile)
+
+export default authRouter
