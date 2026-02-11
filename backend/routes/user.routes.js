bfddfe290ee96ad5ba27cda9e90ae3e06ac639ed
shekhar-narayan-mishra <shekhar.mishra@adypu.edu.in>
2026-02-11 18:32:44 +0530

    🛤️ Clean up user routes
    - Removed guest-specific API route
    - Consolidated route handling
    - Better route organization

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { askToAssistant, getCurrentUser, updateAssistant, clearHistory, askToAssistantGuest } from "../controllers/user.controllers.js"
+import { askToAssistant, getCurrentUser, updateAssistant, clearHistory } from "../controllers/user.controllers.js"
 import isAuth from "../middlewares/isAuth.js"
 import upload from "../middlewares/multer.js"
 
@@ -8,7 +8,6 @@ const userRouter = express.Router()
 userRouter.get("/current", isAuth, getCurrentUser)
 userRouter.post("/update", isAuth, upload.single("assistantImage"), updateAssistant)
 userRouter.post("/asktoassistant", isAuth, askToAssistant)
-userRouter.post("/ask-guest", askToAssistantGuest)
 userRouter.delete("/history", isAuth, clearHistory)
 
 export default userRouter
